5d1f3ac486556dfbef0733dc9d2b8ce286bd90c3
lab-charles <haung7tjsl@gmail.com>
2018-12-20 17:09:10 +0800

    setting sample

---
+++ settings.sample.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+import logging
+
+from colorama import Style
+from colorama import init
+
+# 设置log
+logging.basicConfig(
+    level=logging.INFO,
+    format=''.join([Style.DIM, '[%(asctime)s] ',
+                    Style.NORMAL, '%(message)s',
+                    Style.RESET_ALL]),
+    datefmt='%T',
+)
+logging.getLogger("requests").setLevel(logging.WARNING)
+
+# 命令行
+init(autoreset=True)
+
+# 存储目录
+STORE_PATH = './downloads'
+
+# 在这里粘贴你的cookies
+COOKIES = 'SCF=...;SSOLoginState=...;SUB=...;SUHB=...'
+
+# 在这里添加目标用户的微博主页url
+#  TARGETS = 'https://www.weibo.com/p/10080883d7b8a4ba9ca03c1e863198265ea9fa/topic_album?from=page_100808&mod=TAB#place'
